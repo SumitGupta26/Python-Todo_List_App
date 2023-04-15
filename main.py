60ffa42efc9f2823f8fedf591e788120cc479f3c
nex26dv <sumit.gupta@pb.com>
2023-04-16 01:31:25 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+valueVariable = "Enter a item to add in List: "
+
+todo1 = input(valueVariable)
+todo2 = input(valueVariable)
+todo3 = input(valueVariable)
+
+todoS = [todo1, todo2, todo3]
+
+print(todoS)
